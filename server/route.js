d8f236adc85d4be7912e86511dca52fea4512352
giaat <g.peria@dixeed.com>
2017-11-23 16:01:57 +0100

    externalisation functions routes

--- server/route.js
+++ server/route.js
@@ -1,79 +1,36 @@
 'use strict';
 
-var tasksDb = require('./object');
-var Boom = require('boom');
+const handlers = require('./handlers');
 
 module.exports = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: function(request, reply) {
-            reply('Learn Angular/Node');
-        },
-    },
-
-    {
-        method: 'GET',
-        path: '/tasks',
-        handler: function(request, reply) {
-            reply(tasksDb.getAll());
-        },
-    },
-
-    {
-        method: 'GET',
-        path: '/tasks/{id}',
-        handler: function(request, reply) {
-            if (tasksDb.getById(request.params.id) === undefined) {
-                return reply(Boom.notFound("This task doesn't exist"));
-            }
-            return reply(tasksDb.getById(request.params.id));
-        }
-    },
-
-    {
-        method: 'POST',
-        path: '/tasks/create',
-        handler: function(request, reply) {
-            var newTask = {
-                id: request.payload.id,
-                title: request.payload.title,
-                content: request.payload.content,
-                tags: request.payload.tags,
-            };
-            tasksDb.addTask(newTask);
-            reply("Task added");
-        }
-    },
-
-    {
-        method: 'DELETE',
-        path: '/tasks/delete/{id}',
-        handler: function(request, reply) {
-            if (tasksDb.getById(request.params.id) === undefined) {
-                return reply(Boom.notFound("This task doesn't exist"));
-            }
-            tasksDb.deleteTask(request.params.id);
-            reply("Task deleted");
-        }
-    },
-
-    {
-        method: 'PUT',
-        path: '/tasks/update/{id}',
-        handler: function(request, reply) {
-            if (tasksDb.getById(request.params.id) === undefined) {
-                return reply(Boom.notFound("This task doesn't exist"));
-            }
-            var task = {
-                id: request.params.id,
-                title: request.payload.title,
-                content: request.payload.content,
-                tags: request.payload.tags,
-            };
-
-            tasksDb.updateTask(task);
-            reply("Task updated");
-        }
-    }
+  {
+    method: 'GET',
+    path: '/',
+    handler: handlers.getHelloWord,
+  },
+  {
+    method: 'GET',
+    path: '/tasks',
+    handler: handlers.getAllTasks,
+  },
+  {
+    method: 'GET',
+    path: '/tasks/{id}',
+    handler: handlers.getTaskById,
+  },
+  {
+    method: 'POST',
+    path: '/tasks/create',
+    handler: handlers.createTask,
+  },
+  {
+    method: 'PUT',
+    path: '/tasks/update/{id}',
+    handler: handlers.updateTask,
+  },
+  {
+    method: 'DELETE',
+    path: '/tasks/delete/{id}',
+    handler: handlers.deleteTask,
+  },
 ];
--- server/test/index.js
+++ server/test/index.js
@@ -5,13 +5,12 @@ var tasksDb = require('../object');
 var server = {};
 
 describe('Task [endpoint]:', function() {
-
   // we clear the variable to make sure every test is independent from the others
-  beforeEach('Set up the server', function(){
+  beforeEach('Set up the server', function() {
     server = require('../server');
   });
 
-  afterEach('Close the server', function(done){
+  afterEach('Close the server', function(done) {
     server.stop(function(err) {
       if (err) {
         return done(err);
@@ -22,95 +21,98 @@ describe('Task [endpoint]:', function() {
     // could be simplified to server.stop(done);
   });
 
-  it('should return tasks list when contacted on /tasks', function(done){
+  it('should return tasks list when contacted on /tasks', function(done) {
     var options = {
       method: 'GET',
-      url: '/tasks'
+      url: '/tasks',
     };
 
-    server.inject(options, function(response){
+    server.inject(options, function(response) {
       assert.equal(response.statusCode, 200);
       assert.equal(response.result.constructor, Array);
       done();
     });
   });
 
-  it('should return the complete information of a task referenced by its id when contacted on /task/{id}', function(done){
+  it('should return the complete information of a task referenced by its id when contacted on /task/{id}', function(
+    done
+  ) {
     var options = {
       method: 'GET',
-      url: '/tasks/1'
+      url: '/tasks/1',
     };
 
-    server.inject(options, function(response){
+    server.inject(options, function(response) {
       assert.equal(response.statusCode, 200);
-      assert.equal(response.result,tasksDb.data[1]);
+      assert.equal(response.result, tasksDb.data[1]);
       done();
     });
   });
 
-  it('should return "Task added" when contacted on /task/create with params', function(done){
+  it('should return "Task added" when contacted on /task/create with params', function(done) {
     var options = {
       method: 'POST',
       url: '/tasks/create',
       payload: {
-        id:2,
-        title:"Aller au Berthom",
-        content:"RDV avec les potos",
-        tags:["berthom","biere","saoul"]
-      }
+        id: 2,
+        title: 'Aller au Berthom',
+        content: 'RDV avec les potos',
+        tags: ['berthom', 'biere', 'saoul'],
+      },
     };
 
-    server.inject(options, function(response){
+    server.inject(options, function(response) {
       assert.equal(response.statusCode, 200);
-      assert.equal(response.result,"Task added");
+      assert.equal(response.result, 'Task added');
       done();
     });
   });
 
-  it('should return "Task deleted" when contacted on /task/delete/{id}', function(done){
+  it('should return "Task deleted" when contacted on /task/delete/{id}', function(done) {
     var options = {
       method: 'DELETE',
-      url: '/tasks/delete/2'
+      url: '/tasks/delete/2',
     };
 
-    server.inject(options, function(response){
+    server.inject(options, function(response) {
       assert.equal(response.statusCode, 200);
-      assert.equal(response.result,"Task deleted");
+      assert.equal(response.result, 'Task deleted');
       done();
     });
   });
 
-  it('should return "Task updated" when contacted on /task/update/{id}', function(done){
+  it('should return "Task updated" when contacted on /task/update/{id}', function(done) {
     var options = {
       method: 'PUT',
       url: '/tasks/update/1',
       payload: {
-        id:1,
-        title:"My little task before going to bed",
-        content:"I must wash my clothes and my kitchen before going to bed!",
-        tags:["before","bed","wash","kitchen"]
-      }
+        id: 1,
+        title: 'My little task before going to bed',
+        content: 'I must wash my clothes and my kitchen before going to bed!',
+        tags: ['before', 'bed', 'wash', 'kitchen'],
+      },
     };
 
-    server.inject(options, function(response){
+    server.inject(options, function(response) {
       assert.equal(response.statusCode, 200);
-      assert.equal(response.result,"Task updated");
+      assert.equal(response.result, 'Task updated');
       done();
     });
   });
 
-  it("should return 'This task doesn't exist' when contacted on /task/tasks/{id} when id doesn't exist", function(done){
+  it("should return 'This task doesn't exist' when contacted on /task/tasks/{id} when id doesn't exist", function(
+    done
+  ) {
     var options = {
       method: 'GET',
-      url: '/tasks/22'
+      url: '/tasks/22',
     };
 
-    server.inject(options, function(response){
+    server.inject(options, function(response) {
       var payload = JSON.parse(response.payload);
       assert.equal(payload.statusCode, 404);
-      assert.equal(payload.message,"This task doesn't exist");
+      assert.equal(payload.message, "This task doesn't exist");
       done();
     });
   });
-
 });
